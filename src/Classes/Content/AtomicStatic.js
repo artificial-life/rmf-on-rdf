549842ad274790680bcf67816b440e8f55b71307
artificial-life <proteus.protocol@gmail.com>
2015-10-27 01:06:17 +0600

    fix

--- src/Classes/Content/AtomicStatic.js
+++ src/Classes/Content/AtomicStatic.js
@@ -5,16 +5,20 @@ var AtomicBasic = require('./AtomicBasic.js');
 class AtomicStatic extends AtomicBasic {
     constructor(Model, data) {
         super(Model, data);
-        this.readOnly = false;
+        this.readOnly(false);
     }
     readOnly(value) {
         this.read_only = value;
+    }
+    setDataProvider(provider) {
+        super.setDataProvider(provider);
+        this.readOnly(!provider);
     }
     resolve(params) {
         var initial = super.resolve(params);
 
         //@TODO: data provider or smth like this???
-        //var stored = dataProvider.get(??)
+        //var stored = dataProvider.get(store_accessor);
         //var complete = intial + stored;
         //return complete;
     }
