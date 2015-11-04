db5ec27200fb05ac92c1ed184bfc743c6a04e142
artificial-life <proteus.protocol@gmail.com>
2015-11-04 17:09:49 +0600

    Ideal Error and test

---
+++ tests/externals/Error/DogeError.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+describe('DoggieError', () => {
+    describe('is awesome', () => {
+        it('Yes, its f*cking awesome', () => {
+            expect(DogeError).to.be.ok;
+        })
+    })
+});
