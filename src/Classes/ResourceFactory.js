47e5ba172d22b0f0754b1a97d69ef07a2eb1fb52
sleeping-sun <solaris.null@gmail.com>
2015-11-12 08:11:40 +0600

    ResourceFactory very draft
    Wow! So draft! Wow. Such good ideas.

--- src/Classes/ResourceFactory.js
+++ src/Classes/ResourceFactory.js
@@ -1,16 +1,52 @@
 'use strict'
 
-//extends what?
-class Factory {
-    constructor(formula, ingredients) {
+var Content = require('./Content.js');
 
-    }
-    reserve() {
+class Factory extends Content {
+  constructor() {
+    super();
 
-    }
-    observe() {
+    this.algorithm = () => {
+      throw new Error('algorithm required');
+    };
+  }
+  addStorage(atom) {
+    //@TODO: define AtomicStorage
+    // if (!(atom instanceof AtomicStorage)) throw new Error('only atomic storage can be attached ');
+    return super.addAtom(atom, 'Storage');
+  }
+  addBuilder(algorithm, ...ingredients) {
+    // @TODO: I know, how to deal with it. I'm just going to leave this here for a moment
+    // var ingredients = _.map(this.ingredients, (ingredient) => {
+    //   return ingredient.observe(params);
+    // });
+    //
+    // var raw_data = this.algorithm(...ingredients);
+  }
+  addIngredient(ingredient) {
+    //@TODO: define Ingredient
+    // if (!(ingredient instanceof Ingredient)) throw new Error('ingredient should be child of Ingredient');
+    this.ingredients.push(ingredient);
+    return this;
+  }
+  setAlgorithm(algorithm) {
+    this.algorithm = algorithm.bind(this);
+  }
+  build(params) { //@NOTE: that's specific to factory content
+    return this.selector().id('<namespace>content').id('Builder').resolve(params);
+  }
+  getStorage() {
+    return super.getAtom('<namespace>content', 'Storage')
+  }
+  getBuilder() {
+    return super.getAtom('<namespace>content', 'Builder')
+  }
+  observe(params) {
 
-    }
+  }
+  reserve() {
+
+  }
 }
 
 module.exports = Factory;
