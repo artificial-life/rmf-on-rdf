f694cc5ac499a8d5f6ac7e727f63cc460861b19c
YuuKohaku <heimaolee@gmail.com>
2016-01-15 04:52:11 +0600

    emp state support

--- src/Classes/Atomic/BaseTypes/Employee.js
+++ src/Classes/Atomic/BaseTypes/Employee.js
@@ -4,7 +4,7 @@ let Fieldset = require("./Fieldset");
 
 class Employee extends Fieldset {
 	constructor() {
-		let fields = ["id", "phone", "first_name", "last_name", "middle_name", "login", "password", "provides", "has_membership", "has_schedule", "has_permission"];
+		let fields = ["id", "phone", "first_name", "last_name", "middle_name", "login", "password", "provides", "has_membership", "has_schedule", "has_permission", "state"];
 		super(fields);
 	}
 }
