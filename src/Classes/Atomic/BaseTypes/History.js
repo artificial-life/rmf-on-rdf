71dbc17b71c1a41b7909ca9d0a0add99383d89a9
YuuKohaku <soul.of.bytes@gmail.com>
2016-03-11 15:04:44 +0600

    1

--- src/Classes/Atomic/BaseTypes/History.js
+++ src/Classes/Atomic/BaseTypes/History.js
@@ -4,7 +4,7 @@ let Fieldset = require("./Fieldset");
 
 class History extends Fieldset {
 	constructor() {
-		let fields = ["event_name", "time", "reason", "subject", "object"];
+		let fields = ["event_name", "time", "reason", "subject", "object", "local_time"];
 		super(fields);
 	}
 
